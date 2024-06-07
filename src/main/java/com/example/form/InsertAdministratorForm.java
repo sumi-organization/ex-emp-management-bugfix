2e602e53e0d06f2732d4910c38e634ae75cc0cf6
sumiship <osumi.k.osumi.k@gmail.com>
2024-06-07 13:47:23 +0900

    管理者登録時のバリデーションを追加

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +12,14 @@ package com.example.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank
 	private String name;
 	/** メールアドレス */
+	@NotBlank
+	@Email
 	private String mailAddress;
 	/** パスワード */
+	@Size(min = 8,max = 22)
 	private String password;
 
 	public String getName() {
